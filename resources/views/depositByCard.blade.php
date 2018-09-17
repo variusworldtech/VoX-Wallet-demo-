ef21b5e75b27c7aabc6fc5beea661cece482088b
Shirline Serrao <shirline@variusworldtech.com>
2018-09-17 11:46:15 +0100

    card

--- resources/views/depositByCard.blade.php
+++ resources/views/depositByCard.blade.php
@@ -6,6 +6,8 @@
 <!doctype html>
 <html lang="en">
     <head>
+         {{ print_r($_POST) }}
+        {{ print_r($_GET) }}
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
