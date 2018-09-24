e0385d089b1896dfa1e6e777224174ba6ad95600
Andy Davis <github@andy-davis.co.uk>
2018-09-24 12:46:57 +0100

    stripe load removed on deposit

--- resources/views/deposit.blade.php
+++ resources/views/deposit.blade.php
@@ -48,8 +48,7 @@
                                     </div>
                                 </div>
                                     
-                                    <script src="https://checkout.stripe.com/checkout.js"> 
-                                    </script>
+                                   
                             </div>
                         
                             
