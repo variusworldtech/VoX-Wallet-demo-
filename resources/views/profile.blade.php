d04b3018789c55372c2477b83a13498425304995
Andy Davis <github@andy-davis.co.uk>
2018-09-25 23:28:35 +0100

    links fix

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -18,9 +18,9 @@
 
                             <ul class="icon-list">
                                 <!-- <li><i class="zmdi zmdi-phone"></i></li> -->
-                                <li><i class="zmdi zmdi-email"></i> href="{{ Auth::user()->email }}</li>
-                                <li><i class="zmdi zmdi-twitter"></i>twitter.com/{{ Auth::user()->name }}</li>
-                                <li><i class="zmdi zmdi-facebook"></i>facebook.com/{{ Auth::user()->name }}</li>
+                                <li><i class="zmdi zmdi-email"></i> {{ Auth::user()->email }}</li>
+                                <li><i class="zmdi zmdi-twitter"></i><a href="http://twitter.com/{{ strtolower(explode(" ", Auth::user()->name)[0]) }}">twitter.com/{{ strtolower(explode(" ", Auth::user()->name)[0]) }}</a></li>
+                                <li><i class="zmdi zmdi-facebook"></i><a href="http://facebook.com/{{ strtolower(explode(" ", Auth::user()->name)[1]) }}">facebook.com/{{ strtolower(explode(" ", Auth::user()->name)[1]) }}</a></li>
                                 <li><i class="zmdi zmdi-pin"></i>EC2M 4QD</li>
                             </ul>
                         </div>
